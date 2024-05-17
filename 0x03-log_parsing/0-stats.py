750a64f2900c3447b1c28277ce9e7c8f9b1d5e61
Oussama Afnakkar <124894860+oussamaafnakkar@users.noreply.github.com>
2024-05-17 16:38:20 +0000

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,13 @@
 #!/usr/bin/python3
-""" Log parsing """
+import random
 import sys
-from collections import defaultdict
+import datetime
 
-
-def print_stats(total_size, status_counts):
-    """ Print stats """
-    print("File size: {}".format(total_size))
-    for status_code in sorted(status_counts.keys()):
-        if status_counts[status_code] > 0:
-            print("{}: {}".format(status_code, status_counts[status_code]))
-
-
-def process_line(line, total_size, status_counts):
-    """ Process a line from stdin """
-    parts = line.split()
-    try:
-        status_code = int(parts[-2])
-        file_size = int(parts[-1])
-
-        if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
-            status_counts[status_code] += 1
-            total_size += file_size
-
-    except (ValueError, IndexError):
-        pass
-
-    return total_size, status_counts
-
-
-total_size = 0
-status_counts = defaultdict(int)
-line_count = 0
-
-try:
-    for line in sys.stdin:
-        line_count += 1
-        total_size, status_counts = process_line(
-            line, total_size, status_counts)
-
-        if line_count % 10 == 0:
-            print_stats(total_size, status_counts)
-
-finally:
-    print_stats(total_size, status_counts)
+for i in range(10000):
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
